10ce9b19584048f4acedf8ea458818ec4d533ed5
Celestu <celestuwa@gmail.com>
2023-03-23 07:32:56 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -2,8 +2,8 @@
 #include "variadic_functions.h"
 
 /**
- * sum_them_all - A function with two arguments
- * @n: int data structure - first argument
+ * sum_them_all - function with two arguments
+ * @n: int type first argume t
  *
  * Description: returns the sum of all its parameters
  * Return: value of sum or 0
@@ -11,23 +11,16 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i;
-	int add = 0, number;
-
-	va_list parameters;
-
-	va_start(parameters, n);
+	int sum = 0;
+	va_list arg;
 
 	if (n == 0)
-	{
 		return (0);
-	}
-
+	va_start(arg, n);
 	for (i = 0; i < n; i++)
 	{
-		number = va_arg(parameters, int);
-		add = add + number;
+		sum += va_arg(arg, int);
 	}
-
-	va_end(parameters);
-	return (add);
+	va_end(arg);
+	return (sum);
 }
