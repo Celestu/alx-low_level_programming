32398d5f418639cbde0da588506e4512557f9fcd
Celestu <celestuwa@gmail.com>
2023-03-23 07:25:03 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,90 +1,48 @@
+#include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
-#include "variadic_functions.h"
-/**
-* print_c - print char
-*
-* @s: va_list
-*/
-void print_c(va_list s)
-{
-	printf("%c", va_arg(s, int));
-}
-/**
-* print_i - print integer
-*
-* @s: va_list
-*/
-void print_i(va_list s)
-{
-	printf("%i", va_arg(s, int));
-}
-/**
-* print_f - print float
-*
-* @s: va_list
-*/
-void print_f(va_list s)
-{
-	printf("%f", va_arg(s, double));
-}
-/**
-* print_s - print string
-*
-* @s: va_list
-*/
-void print_s(va_list s)
-{
-	va_list clone;
 
-	va_copy(clone, s);
-	if (va_arg(clone, char*) == '\0')
-	{
-	printf("%p", va_arg(s, char*));
-	}
-	else
-	{
-	printf("%s", va_arg(s, char*));
-	}
-}
 /**
-* print_all - print char, integer, float and string
-*
-* @format: format
-*/
+ * print_all - A function with 2 parameters
+ * @format: char type pointer to string
+ *
+ * Description: prints anything followed by a new line
+ * Return: na
+ */
 void print_all(const char * const format, ...)
 {
+	int j;
+	char *str;
+	char *space;
+	va_list ap;
 
-	va_list list1;
-	unsigned int i = 0, j = 0;
-	my_fmt fmts[] = {
-		{"c", print_c},
-		{"i", print_i},
-		{"f", print_f},
-		{"s", print_s},
-		{'\0', '\0'}
-	};
-
-	va_start(list1, format);
-	while (format[j] != '\0')
+	va_start(ap, format);
+	j = 0;
+	while (format && format[j])
 	{
-		i = 0;
-		while (fmts[i].f != '\0')
+		space = "";
+		if (format[j + 1])
+			space = ", ";
+		switch (format[j])
 		{
-			if (*(fmts[i].f) == format[j])
-			{
-				fmts[i].p(list1);
-				if (format[j + 1] == '\0')
-				{
-					printf("\n");
-				}
-				else
-				{
-					printf(", ");
-				}
-			}
-			i++;
+
+		case 'c':
+			printf("%c%s", va_arg(ap, int), space);
+			break;
+		case 'i':
+			printf("%d%s", va_arg(ap, int), space);
+			break;
+		case 'f':
+			printf("%f%s", va_arg(ap, double), space);
+			break;
+		case 's':
+			str = va_arg(ap, char *);
+			if (!str || !*str)
+				str = "(nil)";
+			printf("%s%s", str, space);
+			break;
 		}
 		j++;
 	}
+	printf("\n");
 }
