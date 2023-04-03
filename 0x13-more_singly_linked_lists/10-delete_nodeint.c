5bec3dfc47674d391863c5783cb2dc7b521ee62b
Celestu <celestuwa@gmail.com>
2023-04-03 11:41:15 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,39 +1,40 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - deletes a node in a linked list at a certain index
- * @head: pointer to the first element in the list
- * @index: index of the node to delete
+ * delete_nodeint_at_index - deletes the node at index of a linked list
+ * @head: pointer to the struct
+ * @index: index of the node that should be deleted
  *
- * Return: 1 (Success), or -1 (Fail)
+ * Return: 1 successful, -1 if not successful
  */
+
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *temp = *head;
-	listint_t *current = NULL;
-	unsigned int i = 0;
+	listint_t *tmp, *pretmp = *head;
+	unsigned int i;
 
-	if (*head == NULL)
+	if (pretmp == NULL)
+	{
 		return (-1);
+	}
 
 	if (index == 0)
 	{
 		*head = (*head)->next;
-		free(temp);
+		free(pretmp);
 		return (1);
 	}
 
-	while (i < index - 1)
+	for (i = 0 ; i < (index - 1) ; i++)
 	{
-		if (!temp || !(temp->next))
+		if (pretmp->next == NULL)
+		{
 			return (-1);
-		temp = temp->next;
-		i++;
+		}
+		pretmp = pretmp->next;
 	}
-
-
-	current = temp->next;
-	temp->next = current->next;
-	free(current);
-
+	tmp = pretmp->next;
+	pretmp->next = tmp->next;
+	free(tmp);
 	return (1);
+}
